0de068f2932f6ee5b59c89a12e604cce119c8c07
s1ddy <siddharthsmart40@gmail.com>
2021-11-06 12:08:23 +0530

    adding result

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const PlayerSelection = 'Rock'
+const ComputerSelection = 'ComputerOutput'
+let result 
 
 const ComputerOutput= () => {
     const RandomNumber = (Math.floor(Math.random() * 3))
@@ -13,4 +16,30 @@ const ComputerOutput= () => {
 
 }
 
-console.log(ComputerOutput())
+
+function PlayRound (PlayerSelection,ComputerSelection) {
+    if (PlayerSelection == ComputerSelection) {
+        result = 'Tie'    
+    }
+    else if (PlayerSelection == 'Scissors' && ComputerSelection == 'Paper'){
+        result = 'You have won , Scissors beats Paper! :)'
+    }
+    else if (PlayerSelection == 'Scissors' && ComputerSelection == 'Rock'){
+        result = 'You have lost, Scissors loses to Rock :('
+    }
+    else if (PlayerSelection == 'Rock' && ComputerSelection == 'Scissors'){
+        console.log(PlayRound(PlayerSelection,ComputerSelection))
+        result = 'You have won, Rock beats Scissors!:)'
+    }
+    else if (PlayerSelection == 'Rock' && ComputerSelection == 'Paper'){
+        result = 'You have lost, Rock loses to Paper :('
+    }
+    else if (PlayerSelection == 'Paper' && ComputerSelection == 'Rock'){
+        result = 'You have won, Paper beats Rock! :)'
+    }
+    else if (PlayerSelection == 'Paper' && ComputerSelection == 'Scissors'){
+        result = 'You have lost, Paper loses to Scissors :('
+    }
+}
+
+console.log(PlayRound(PlayerSelection,ComputerSelection))
